1376e1afbaf4475cc702e4e17072b7f76927d3bf
hasasikis <1243442778@qq.com>
2021-07-13 16:54:18 +0800

    first upload

---
+++ src/main/java/test2.java
@@ -0,0 +1,7 @@
+public class test2 {
+    public static void main(String[] args) {
+        boolean flag = false;
+        int time = 110;
+        System.out.println( flag ?time:time*1.36 + "分钟");
+    }
+}
